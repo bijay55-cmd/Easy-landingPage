1dde86210eb9925971dd6851a0b63fb3400702e8
bijay55-cmd <71406824+bijay55-cmd@users.noreply.github.com>
2020-12-24 12:31:17 +0000

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+//Navbar
+const burger = document.getElementById('burger');
+const ul = document.querySelector('nav ul');
+
+burger.addEventListener('click', () =>{
+    ul.classList.toggle('show');
+});
